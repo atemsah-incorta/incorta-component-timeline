1dd7ac80225f78496a468181357cda74a9d2825b
Abd El-Rahman Tarek El-Temsah <abdulrahman.tarek@incorta.com>
2022-08-29 00:46:34 +0200

    Support custom date formats

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -42,15 +42,15 @@ ChartJS.register(
   Legend
 )
 
-const generateDefaultTooltip = (entry: any[]): string => {
-  const start = format(new Date(entry[0].value), 'yyyy-MM-dd HH:mm:ss')
-  const end = format(new Date(entry[0].value), 'yyyy-MM-dd HH:mm:ss')
-  return start + " to " + end
-}
-
 const Timeline = ({ context, prompts, data, drillDown }: Props) => {
   
   const pallete = context.app.color_palette
+  const settings = context.component.settings
+
+  const isValidFormat = (f: string): boolean => {
+    try { format(new Date(), settings?.format) } catch (error) { return false }
+    return true
+  }
 
   // Mapping the query response to a more usable state 
   const raw: DataSetEntry[] = data.data.map(entry => {
@@ -58,18 +58,35 @@ const Timeline = ({ context, prompts, data, drillDown }: Props) => {
       start: new Date(entry[0].value).getTime(),
       end: new Date(entry[1].value).getTime(),
       label: entry[2].value,
-      tooltip: entry[3]?.value ? entry[3].value : generateDefaultTooltip(entry)
+      tooltip: entry[3]?.value ? entry[3].value : `${entry[0].formatted} → ${entry[1].formatted}`
     }
   })
   .filter(o => o.start < o.end) //Remove weird cases where start > end
   .sort((a, b) => a.start - b.start) //Ensure chronological order by sorting on start
 
-  const mapper: DataSetMapper = context.component.settings?.isWaterfall ? new WaterfallTimelineMapper(raw, pallete) : new GroupedTimelineMapper(raw, pallete)
+  const mapper: DataSetMapper = settings?.isWaterfall ? new WaterfallTimelineMapper(raw, pallete) : new GroupedTimelineMapper(raw, pallete)
   const mappedSet = mapper.generate()
 
   const chartData = {
     labels: mappedSet.labels,
     datasets: mappedSet.dataSets
+  }
+
+  const useCustomFormat = isValidFormat(settings?.format)
+  const customDisplayFormats = {
+    millisecond: settings?.format,
+    second: settings?.format,
+    minute: settings?.format,
+    hour: settings?.format,
+    day: settings?.format,
+    week: settings?.format,
+    month: settings?.format,
+    quarter: settings?.format,
+    year: settings?.format
+  }
+
+  const timeAxisOptions = {
+    unit: settings?.unit === "default" ? false : settings?.unit
   }
 
   const chartOptions = {
@@ -83,13 +100,15 @@ const Timeline = ({ context, prompts, data, drillDown }: Props) => {
         type: 'time' as const,
         parsing: false,
         time: {
-          unit: context.component.settings?.unit === "default" ? false : context.component.settings?.unit
+          unit: settings?.unit === "default" ? false : context.component.settings?.unit,
+          ...(useCustomFormat) && { displayFormats: customDisplayFormats }
         }
       }
     },
     plugins: {
       legend: { display: false },
       tooltip: {
+        displayColors: false,
         callbacks: {
           label: (ctx: any): string => { return ctx.raw.tooltip }
         }
